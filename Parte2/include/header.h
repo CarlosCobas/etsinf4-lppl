61d5116f7e26f5de5aa2278ddb2a2ef27dfe9afa
Carlos Galindo <carlos.s.galindo@gmail.com>
2017-10-31 14:10:11 +0100

    Parte2: eliminados espacios finales y declaradas constantes para operaciones

--- Parte2/include/header.h
+++ Parte2/include/header.h
@@ -10,6 +10,44 @@
 #define TRUE  1
 #define FALSE 0
 #define TALLA_TIPO_SIMPLE 1
+/***************************************************** Constantes operadores */
+/* Operador Asignacion */
+#define OP_ASIG       0
+#define OP_ASIG_SUMA  1
+#define OP_ASIG_RESTA 2
+#define OP_ASIG_MULT  3
+#define OP_ASIG_DIV   4
+/* Operador Logico*/
+#define OP_AND 0
+#define OP_OR  1
+/* Operador igualdad */
+#define OP_IGUAL    0
+#define OP_NOTIGUAL 1
+/* Operador relacional */
+#define OP_MAYOR   0
+#define OP_MAYORIG 1
+#define OP_MENOR   2
+#define OP_MENORIG 3
+/* Operador aditivo */
+#define OP_SUMA  0
+#define OP_RESTA 1
+/* Operador multiplicativo */
+#define OP_MULT 0
+#define OP_DIV  1
+#define OP_MOD  2
+/* Operador unario */
+#define OP_MAS   0
+#define OP_MENOS 1
+#define OP_NOT   2
+/* Operador incremento */
+#define OP_INC 0
+#define OP_DEC 1
+
+/************************************************ Struct para las expresions */
+struct exp {
+    int valor;
+    int tipo;
+} EXP;
 /************************************* Variables externas definidas en el AL */
 extern FILE *yyin;
 extern int   yylineno;
@@ -17,6 +55,11 @@ extern char *yytext;
 /********************* Variables externas definidas en el Programa Principal */
 extern int verbosidad;              /* Flag para saber si se desea una traza */
 extern int numErrores;              /* Contador del numero de errores        */
+
+/************************ Variables externas definidas en Programa Principal */
+extern int verTDS;               /* Flag para saber si se desea imprimir TDS */
+/***************************** Variables externas definidas en las librerias */
+extern int dvar;                     /* Contador del desplazamiento relativo */
 
 #endif  /* _HEADER_H */
 /*****************************************************************************/
--- Parte2/include/libtds.h
+++ Parte2/include/libtds.h
@@ -30,21 +30,21 @@ int dvar;                     /* Desplazamiento en el Segmento de Variables  */
 
 /************************************* Operaciones para la gestion de la TDS */
 int insertarTDS(char *nom, int tipo, int desp, int ref) ;
-/* Inserta en la TDS toda la informacion asociada con un simbolo de: nombre 
+/* Inserta en la TDS toda la informacion asociada con un simbolo de: nombre
    "nom"; tipo "tipo"; desplazamiento relativo en el segmento de variables
-   "desp" y referencia ``ref'' a una posible subtabla de vectores (-1 si no 
-   referencia a otras subtablas). Si el identificador ya existe 
+   "desp" y referencia ``ref'' a una posible subtabla de vectores (-1 si no
+   referencia a otras subtablas). Si el identificador ya existe
    devuelve el valor "FALSE=0" ("TRUE=1" en caso contrario).                 */
 int insertaTDArray (int telem, int nelem) ;
-/* Inserta en la Tabla de Arrays la informacion de un array cuyos elementos 
-   son de tipo "telem" y el numero de elementos es "nelem". Devuelve su 
+/* Inserta en la Tabla de Arrays la informacion de un array cuyos elementos
+   son de tipo "telem" y el numero de elementos es "nelem". Devuelve su
    referencia en la Tabla de Arrays.                                         */
 SIMB obtenerTDS (char *nom) ;
 /* Obtiene toda la informacion asociada con un objeto de nombre "nom" y la
-   devuelve en una estructura de tipo "SIMB" (ver "libtds.h"). Si el objeto 
+   devuelve en una estructura de tipo "SIMB" (ver "libtds.h"). Si el objeto
    no está declarado, en el campo "tipo" devuelve "T_ERROR".                 */
 DIM obtenerInfoArray (int ref) ;
-/* Devuelve toda la informacion asociada con un array referenciado por "ref" 
+/* Devuelve toda la informacion asociada con un array referenciado por "ref"
    en la Tabla de Arrays.                                                    */
 void mostrarTDS () ;
 /* Muestra toda la informacion de la TDS.                                    */
